0882e4437a014696dd955ef848bcc9f140beb818
Nicola Corna <nicola@corna.info>
2015-08-13 23:26:14 +0200

    iio: humidity: si7020: added No Hold read mode
    The Si7013/20/21 modules support 2 read modes:
     * Hold mode (blocking), where the device stretches the clock until the end
    of the measurement
     * No Hold mode (non-blocking), where the device replies NACK for every I2C
    call during the measurement
    Here the No Hold mode is implemented, selectable with the blocking_io
    variable within si7020_platform_data. The default mode is Hold, unless the
    adapter does not support clock stretching, in which case the No Hold mode
    is used.
    
    Signed-off-by: Nicola Corna <nicola@corna.info>

---
+++ include/linux/platform_data/si7020.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015 Nicola Corna <nicola@corna.info>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __SI7020_H_
+#define __SI7020_H_
+
+struct si7020_platform_data {
+	bool blocking_io;
+};
+#endif /* __SI7020_H_ */
